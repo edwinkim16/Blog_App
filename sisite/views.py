8054e865a18d202f8b2fbd22a96ce92e227b095b
edwinkim16 <edwinkimathi226@gmail.com>
2025-09-06 21:43:44 +0300

    home view

--- sisite/views.py
+++ sisite/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.http import JsonResponse
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url="/login/")
+def home(request):
+    emenities = Emenitites.objects.all()
+    context = {'emenities': emenities}
+    return render(request, 'home.html', context)
