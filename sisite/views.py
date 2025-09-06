363856f65a8ec2726ce5b37a3333a6e28ad18dcc
edwinkim16 <edwinkimathi226@gmail.com>
2025-09-06 22:23:10 +0300

    blogs view

--- sisite/views.py
+++ sisite/views.py
@@ -12,3 +12,32 @@ def home(request):
     emenities = Emenitites.objects.all()
     context = {'emenities': emenities}
     return render(request, 'home.html', context)
+
+@login_required(login_url="/login/")
+def api_blogs(request):
+    blogs_objs = Blog.objects.all()
+   
+    price = request.GET.get('price')
+    if price :
+        blogs_objs = blogs_objs.filter(price__lte=price)  
+        
+    emenities = request.GET.get('emenities')
+    if emenities:
+        emenities = emenities.split(',')
+        em = []
+        for e in emenities:
+            try:
+                em.append(int(e))
+            except Exception as e:
+                pass
+        # print(em)
+        blogs_objs = blogs_objs.filter(emenities__in=em).distinct()
+    payload = []
+    for blog_obj in blogs_objs:
+        result = {}
+        result['blog_name'] = blog_obj.blog_name
+        result['blog_description'] = blog_obj.blog_description
+        result['price'] = blog_obj.price
+        payload.append(result)
+        
+    return JsonResponse(payload, safe=False)
